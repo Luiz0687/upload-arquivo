18250074a498cdf736826b4df1ede85c90632e15
Luiz_Guilherme <luiz.2022310970@aluno.iffar.edu.br>
2024-05-28 17:14:19 -0300

    ..

--- upload.php
+++ upload.php
@@ -8,3 +8,10 @@ if ($_FILES['arquivo']['size'] > 2000000){
     echo "O tamanho do arquivo é maior que o limite permitido.";
     die();
 }
+
+//verificar se o arquivo é uma imagem
+var_dump($_FILES['arquivo']['name']);
+var_dump(pathinfo($_FILES['arquivo']['name'],PATHINFO_EXTENSION));
+var_dump(pathinfo($_FILES['arquivo']['name'],PATHINFO_FILENAME));
+var_dump(pathinfo($_FILES['arquivo']['name'],PATHINFO_BASENAME));
+var_dump(pathinfo($_FILES['arquivo']['name'],PATHINFO_DIRNAME));
