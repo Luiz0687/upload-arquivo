1968a1aed68acf816d65fd8d6bcf88aeead76693
Luiz_Guilherme <luiz.2022310970@aluno.iffar.edu.br>
2024-05-28 17:01:30 -0300

    .

--- upload.php
+++ upload.php
@@ -1,3 +1,10 @@
 <?php
 $pastaDestino = "/uploads/";
-var_dump($_POST);
+var_dump($_FILES);
+
+var_dump($_FILES['arquivo']['size']);
+//verificar o tamanho do arquivo é maior que 2 MB
+if ($_FILES['arquivo']['size'] > 2000000){
+    echo "O tamanho do arquivo é maior que o limite permitido.";
+    die();
+}
