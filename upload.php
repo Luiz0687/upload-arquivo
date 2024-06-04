2e4da441da0cd2c2bc6bffc949f343275ca3d3a5
Luiz_Guilherme <luiz.2022310970@aluno.iffar.edu.br>
2024-06-04 16:23:11 -0300

    Update upload.php
    ver se o arquivo é uma imagem

--- upload.php
+++ upload.php
@@ -10,8 +10,34 @@ if ($_FILES['arquivo']['size'] > 2000000){
 }
 
 //verificar se o arquivo é uma imagem
-var_dump($_FILES['arquivo']['name']);
+//var_dump($_FILES['arquivo']['name']);
 var_dump(pathinfo($_FILES['arquivo']['name'],PATHINFO_EXTENSION));
-var_dump(pathinfo($_FILES['arquivo']['name'],PATHINFO_FILENAME));
-var_dump(pathinfo($_FILES['arquivo']['name'],PATHINFO_BASENAME));
-var_dump(pathinfo($_FILES['arquivo']['name'],PATHINFO_DIRNAME));
+$extensao = pathinfo($_FILES['arquivo']['name'],PATHINFO_EXTENSION);
+
+if ( $extensao != "png" && $extensao != "jpg" && 
+     $extensao != "jpeg" && $extensao != "gif" &&
+     $extensao != "jfif" && $extensao != "svg"
+     {
+        echo "O arquivo não é uma imagem! apenas selecione arquivos " .
+        "com extensão png, jpg, jpeg, gif,jfif ou svg.";
+        die();
+     }
+)
+//verifica se é uma imagem de fato
+if (getimagesize($_FILES['arquivo']['tmp_name']) === false ){
+    echo "problemas ao enviar a imagem. Tente novamente.";
+    die();
+}
+//se tudo der certo até aqui, faz o upload
+$fezUpload = move_uploaded_file($_FILES['arquivo']['tmp_name'], 
+$pastaDestino . $_FILES['arquivo']['name']);
+    if ($fezUpload == true){
+        header("location:index.php");
+    } else {
+        echo "Erro ao mover arquivo.";
+    }
+
+
+//var_dump(pathinfo($_FILES['arquivo']['name'],PATHINFO_FILENAME));
+//var_dump(pathinfo($_FILES['arquivo']['name'],PATHINFO_BASENAME));
+//var_dump(pathinfo($_FILES['arquivo']['name'],PATHINFO_DIRNAME));
