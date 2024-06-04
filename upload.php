34b80d195a6be3e249a14d56b7b4c07aa914b156
Luiz_Guilherme <luiz.2022310970@aluno.iffar.edu.br>
2024-06-04 17:08:03 -0300

    faca uploads do mesmo nome

--- upload.php
+++ upload.php
@@ -1,8 +1,8 @@
 <?php
 $pastaDestino = "/uploads/";
-var_dump($_FILES);
+//var_dump($_FILES);
 
-var_dump($_FILES['arquivo']['size']);
+//var_dump($_FILES['arquivo']['size']);
 //verificar o tamanho do arquivo é maior que 2 MB
 if ($_FILES['arquivo']['size'] > 2000000){
     echo "O tamanho do arquivo é maior que o limite permitido.";
@@ -11,26 +11,29 @@ if ($_FILES['arquivo']['size'] > 2000000){
 
 //verificar se o arquivo é uma imagem
 //var_dump($_FILES['arquivo']['name']);
-var_dump(pathinfo($_FILES['arquivo']['name'],PATHINFO_EXTENSION));
-$extensao = pathinfo($_FILES['arquivo']['name'],PATHINFO_EXTENSION);
+//var_dump(pathinfo($_FILES['arquivo']['name'],PATHINFO_EXTENSION));
+$extensao = strtolower(pathinfo($_FILES['arquivo']['name'],PATHINFO_EXTENSION));
 
 if ( $extensao != "png" && $extensao != "jpg" && 
      $extensao != "jpeg" && $extensao != "gif" &&
      $extensao != "jfif" && $extensao != "svg"
+)
      {
         echo "O arquivo não é uma imagem! apenas selecione arquivos " .
         "com extensão png, jpg, jpeg, gif,jfif ou svg.";
         die();
      }
-)
+
 //verifica se é uma imagem de fato
 if (getimagesize($_FILES['arquivo']['tmp_name']) === false ){
     echo "problemas ao enviar a imagem. Tente novamente.";
     die();
 }
+$nomeArquivo = uniqid();
+
 //se tudo der certo até aqui, faz o upload
 $fezUpload = move_uploaded_file($_FILES['arquivo']['tmp_name'], 
- __DIR__ . $pastaDestino . $_FILES['arquivo']['name']);
+ __DIR__ . $pastaDestino . $nomeArquivo . "." . $extensao);
     if ($fezUpload == true){
         header("location:index.php");
     } else {
